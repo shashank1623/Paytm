084ccd3405bb303e73d99fc5b1e3879e0a1e764b
shashank1623 <shashankbhardwaj2030@gmail.com>
2024-06-11 09:26:18 +0530

    Router issue fixed

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -3,10 +3,11 @@
 
 const express = require("express");
 const userRouter = require("./user");
+const accountRouter = require("./account");
 const router = express.Router();
 
-router.get("/user", userRouter); 
-
+router.use("/user", userRouter); 
+router.use("/account",accountRouter)
 
 module.exports = router;
 
