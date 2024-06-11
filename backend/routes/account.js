7fefd7b6de34f1af4665b8b4b9884c8db1e65250
shashank1623 <shashankbhardwaj2030@gmail.com>
2024-06-11 12:54:26 +0530

    Endpoint added user's account balance

--- backend/routes/account.js
+++ backend/routes/account.js
@@ -1,7 +1,49 @@
 const express = require("express");
+const authMiddleware = require("../middleware");
+const { Account } = require("../db");
 const router = express.Router();
 
+// 1. An endpoint for user to get their balance.
+// Method: GET
+// Route: /api/v1/account/balance
+// Response:
+// Status code - 200
 
+router.get("/balance",authMiddleware , async (req,res)=>{
+    const account = await Account.findOne({userId : req.userId});
+
+    res.json({
+        balance : account.balance
+    })
+})
+
+// 2. An endpoint for user to transfer money to another account
+// Method: POST
+// Route: /api/v1/account/transfer
+// Body
+// {
+//	to: string,
+//	amount: number
+//}
+
+// Response:
+// Status code - 200
+// {
+//	message: "Money transferred successfully"
+//}
+
+// Status code - 400
+// {
+//	message: "Insufficient balance"
+//}
+// status code - 400
+// {
+//	message: "Invalid account"
+//}
+
+router.post("/transfer",authMiddleware , async(req,res)=>{
+    
+})
 
 
 module.exports = router;
