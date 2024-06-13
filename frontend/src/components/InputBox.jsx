19c03b3e3e149e76fd81e617170abe3be37313c3
shashank1623 <shashankbhardwaj2030@gmail.com>
2024-06-13 23:26:28 +0530

    Added Border color for visibilty

--- frontend/src/components/InputBox.jsx
+++ frontend/src/components/InputBox.jsx
@@ -6,6 +6,6 @@ export function InputBox({label,placeholder,onChange}){
             {label}
 
         </div>
-        <input onChange={onChange} placeholder={placeholder} className="w-full px-2 py-1 rounded border-slate-200" />
+        <input onChange={onChange} placeholder={placeholder} className="w-full px-2 py-1 border rounded border-slate-200" />
     </div>
 }
