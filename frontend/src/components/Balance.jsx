705b1e6124040c588fe0236c7b6dda9bd3d709c2
shashank1623 <shashankbhardwaj2030@gmail.com>
2024-06-14 13:10:00 +0530

    User Dashboard partial component Added

---
+++ frontend/src/components/Balance.jsx
@@ -0,0 +1,8 @@
+// eslint-disable-next-line
+export const Balance = ({value}) =>{
+    return <div className="flex">
+        <div className="font-bold text-lg">
+            $ {value}
+        </div>
+    </div>
+}
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,8 @@
+
+
+export const Home = ()=>{
+    return <div className="bg-slate-300 h-screen flex justify-center">
+       
+        
+    </div>
+}
