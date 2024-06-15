46123242561a12265aacddccc74b85e850e6a65d
shashank1623 <shashankbhardwaj2030@gmail.com>
2024-06-15 23:09:22 +0530

    /dashboard access denined to the aunthorized Users

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import {Signup} from "./pages/Signup"
 import {Signin} from "./pages/Signin"
 import {Home} from "./pages/Home"
 import { Dashboard } from './pages/Dashboard'
+import {PrivateRoute} from './components/PrivateRoute'
 function App() {
 
   return (
@@ -12,7 +13,7 @@ function App() {
         <Route path="/" element={<Home/>}></Route>
         <Route path='/signup' element={<Signup/>}></Route>
         <Route path="/signin" element={<Signin/>}></Route>
-        <Route path='/dashboard' element={<Dashboard/>}></Route>
+        <Route path='/dashboard' element={<PrivateRoute element={<Dashboard/>}/>}></Route>
       </Routes>
       </BrowserRouter>
     </>
