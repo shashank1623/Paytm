3036d443b2f896227a42a400c2672a173a244ba0
shashank1623 <shashankbhardwaj2030@gmail.com>
2024-06-16 10:43:08 +0530

    State Managment added to sign in route

--- frontend/src/pages/Signin.jsx
+++ frontend/src/pages/Signin.jsx
@@ -3,7 +3,15 @@ import { SubHeading } from "../components/SubHeading";
 import { InputBox } from "../components/InputBox";
 import { Button } from "../components/Button";
 import { BottomWarning } from "../components/BottomWarning";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+
 export const Signin = () =>{
+
+  const [username,setUsername] = useState("");
+  const [password,setPassword] = useState("");
+  const navigate = useNavigate();
 
   return <div className="bg-slate-300 h-screen flex justify-center">
     <div className="flex flex-col justify-center">
@@ -11,11 +19,24 @@ export const Signin = () =>{
         <Heading label={"Sign in"}/>
         <SubHeading label={"Enter you credentials to acess your account"}/>
 
-        <InputBox placeholder="jhonDoe@gamil.com" label={"Email"}/>
-        <InputBox placeholder="*******" label={"Password"}/>
+        <InputBox onChange={e=>{
+                    setUsername(e.target.value)
+                }} placeholder="jhonDoe@gamil.com" label={"Email"}/>
+        <InputBox onChange={e=>{
+                    setPassword(e.target.value)
+                }} placeholder="*******" label={"Password"}/>
 
         <div className="pt-4">
-          <Button label={"Sign in"}/>
+          <Button onClick={async ()=>{
+                        const response = await axios.post("http://localhost:8080/api/v1/user/signin",{
+                            username,
+                            password
+                        });
+
+                        localStorage.setItem("token",response.data.token)
+                        navigate("/dashboard")
+
+                    }} label={"Sign in"}/>
         </div>
 
         <BottomWarning label={"Don't have account?"} buttonText={"Sign Up"} to={"/signup"}/>
