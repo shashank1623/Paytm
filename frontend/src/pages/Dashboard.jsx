e6f0fdcb14f574c5de65ddcda499b1627f61ba24
shashank1623 <shashankbhardwaj2030@gmail.com>
2024-06-17 20:50:16 +0530

    User balance Local storage issue fixed

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -11,7 +11,7 @@ export const Dashboard = () =>{
 
     useEffect(() => {
         const fetchBalance = async () => {
-            const token = localStorage.getItem("authToken");
+            const token = localStorage.getItem("token");
 
             if (!token) {
                 console.error("No auth token found");
@@ -19,13 +19,13 @@ export const Dashboard = () =>{
             }
 
             try {
-                const response = await axios.get("/balance", {
+                const response = await axios.get("http://localhost:8080/api/v1/account/balance", {
                     headers: {
                         "Authorization": `Bearer ${token}`,
                         "Content-Type": "application/json"
                     }
                 });
-
+                //console.log("Balance response:", response.data);
                 setBalance(response.data.balance);
             } catch (error) {
                 console.error("Error fetching balance:", error);
