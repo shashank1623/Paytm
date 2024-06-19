b79e831d0386c5e405e54dc68cbbcfd84e2b9336
shashank1623 <shashankbhardwaj2030@gmail.com>
2024-06-19 22:06:00 +0530

    Home page boilerplate added

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,8 +1,7 @@
 
-
+import { BottomWarning } from "../components/BottomWarning";
 export const Home = ()=>{
     return <div className="bg-slate-300 h-screen flex justify-center">
-       
-        
+       <BottomWarning label={"Don't have account?"} buttonText={"Sign Up"} to={"/signup"}/>
     </div>
 }
